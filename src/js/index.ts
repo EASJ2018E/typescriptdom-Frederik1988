e09ee9e52e8c01dc21b93f74ecfe2e58f3f19fff
Frederik Skærbæk Jensen <fred15b4@edu.easj.dk>
2018-09-30 15:28:04 +0200

    For loop afprøvet og nodeliste udskrevet i consolen

--- src/js/index.ts
+++ src/js/index.ts
@@ -7,4 +7,9 @@ console.log(x.length)
 let x1 = document.getElementsByTagName("li")
 console.log(x1)
 
-let x2 
+let result = document.getElementsByClassName("sundt")
+for (let i = 0; i < result.length; i++){
+    let bla = result[i]
+    console.log(bla)
+
+}
